317b1cab9d00b8e071b575ccff3aea85dd921eca
chicheong <chicheong@gmail.com>
2017-05-31 12:30:51 +0800

    Project initial

---
+++ src/main/java/com/wongs/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.wongs.config.audit;
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { MallSharedModule, UserRouteAccessService } from './shared';
+import { MallHomeModule } from './home/home.module';
+import { MallAdminModule } from './admin/admin.module';
+import { MallAccountModule } from './account/account.module';
+import { MallEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        MallSharedModule,
+        MallHomeModule,
+        MallAdminModule,
+        MallAccountModule,
+        MallEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class MallAppModule {}
