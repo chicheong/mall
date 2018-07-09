2d4b8bc1cbe54a0e836bd7f92b0695deada6c94d
chicheong <chicheong@gmail.com>
2018-07-09 18:18:00 +0800

    Development in Progress

--- src/main/webapp/app/entities/my-order/cart/cart-payment/cart-payment.component.ts
+++ src/main/webapp/app/entities/my-order/cart/cart-payment/cart-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -10,13 +10,65 @@ import { MyOrderService } from './../../my-order.service';
 
 import { CartComponent } from './../../cart.component';
 
+declare let paypal: any;
+
 @Component({
     selector: 'jhi-payment',
     templateUrl: './cart-payment.component.html'
 })
-export class CartPaymentComponent extends CartComponent implements OnInit, OnDestroy {
+export class CartPaymentComponent extends CartComponent implements OnInit, OnDestroy, AfterViewChecked {
 
     isSaving: boolean;
+    selectedMethod: string;
+
+/********** For Paypal starts **********/
+    addScript = false;
+    paypalLoad = true;
+    paypalConfig = {
+        env: 'sandbox',
+        client: {
+            sandbox: '<your-sandbox-key-here>',
+            production: '<your-production-key here>'
+        },
+        style: {
+            color: 'gold',   // 'gold, 'blue', 'silver', 'black'
+            size:  'responsive', // 'medium', 'small', 'large', 'responsive'
+            shape: 'rect'    // 'rect', 'pill'
+        },
+        commit: true,
+        payment: (data, actions) => {
+            return actions.payment.create({
+                payment: {
+                    transactions: [
+                                   { amount: { total: this.myOrder.total, currency: 'INR' } }
+                    ]
+                }
+            });
+        },
+        onAuthorize: (data, actions) => {
+            return actions.payment.execute().then((payment) => {
+                // Do something when payment is successful.
+            });
+        }
+    };
+    ngAfterViewChecked(): void {
+        if (!this.addScript) {
+            this.addPaypalScript().then(() => {
+                paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+                this.paypalLoad = false;
+            });
+        }
+    }
+    addPaypalScript() {
+        this.addScript = true;
+        return new Promise((resolve, reject) => {
+            const scripttagElement = document.createElement('script');
+            scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+            scripttagElement.onload = resolve;
+            document.body.appendChild(scripttagElement);
+        });
+    }
+/********** For Paypal ends **********/
 
     constructor(
         protected eventManager: JhiEventManager,
@@ -28,6 +80,16 @@ export class CartPaymentComponent extends CartComponent implements OnInit, OnDes
     ngOnInit() {
         super.ngOnInit();
         this.isSaving = false;
+    }
+
+    onSelectionChange(entry) {
+        if (entry === 'credit') {
+
+        } else if (entry === 'paypal') {
+
+        } else if (entry === 'payme') {
+
+        }
     }
 
     save() {
