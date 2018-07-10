af1c1a86006c33f01071a6827e3d253a5c4a3850
chicheong <chicheong@gmail.com>
2018-07-10 18:21:27 +0800

    Development in Progress

--- src/main/webapp/app/entities/my-order/cart/cart-payment/cart-payment.component.ts
+++ src/main/webapp/app/entities/my-order/cart/cart-payment/cart-payment.component.ts
@@ -27,27 +27,81 @@ export class CartPaymentComponent extends CartComponent implements OnInit, OnDes
     paypalConfig = {
         env: 'sandbox',
         client: {
-            sandbox: '<your-sandbox-key-here>',
+            sandbox: 'AYHgQIpqQCh3rjX5kTFawz6NB0GwNWlQqW4i8WTlMNHAOmQIRUdT4djZhBwq4Kt0nPjpTBhRvbTm-cqS',
             production: '<your-production-key here>'
         },
         style: {
             color: 'gold',   // 'gold, 'blue', 'silver', 'black'
             size:  'responsive', // 'medium', 'small', 'large', 'responsive'
-            shape: 'rect'    // 'rect', 'pill'
+            shape: 'pill'    // 'rect', 'pill'
         },
         commit: true,
         payment: (data, actions) => {
             return actions.payment.create({
                 payment: {
                     transactions: [
-                                   { amount: { total: this.myOrder.total, currency: 'INR' } }
-                    ]
+                       {
+                           amount: {
+                               total: '30.11',
+                               currency: 'HKD',
+                               details: {
+                                   subtotal: '30.00',
+                                   tax: '0.07',
+                                   shipping: '0.03',
+                                   handling_fee: '1.00',
+                                   shipping_discount: '-1.00',
+                                   insurance: '0.01'
+                               }
+                           },
+                           description: 'The payment transaction description.',
+                           custom: '90048630024435',
+                           // invoice_number: '12345', Insert a unique invoice number
+                           payment_options: {
+                               allowed_payment_method: 'INSTANT_FUNDING_SOURCE'
+                           },
+                           soft_descriptor: 'ECHI5786786',
+                           item_list: {
+                               items: [
+                                   {
+                                     name: 'hat',
+                                     description: 'Brown hat.',
+                                     quantity: '5',
+                                     price: '3',
+                                     tax: '0.01',
+                                     sku: '1',
+                                     currency: 'HKD'
+                                   },
+                                   {
+                                     name: 'handbag',
+                                     description: 'Black handbag.',
+                                     quantity: '1',
+                                     price: '15',
+                                     tax: '0.02',
+                                     sku: 'product34',
+                                     currency: 'HKD'
+                                   }
+                              ],
+                              shipping_address: {
+                                   recipient_name: 'Brian Robinson',
+                                   line1: '4th Floor',
+                                   line2: 'Unit #34',
+                                   city: 'San Jose',
+                                   country_code: 'US',
+                                   postal_code: '95131',
+                                   phone: '011862212345678',
+                                   state: 'CA'
+                              }
+                           }
+                       }
+                    ],
+                    note_to_payer: 'Contact us for any questions on your order.'
                 }
             });
         },
         onAuthorize: (data, actions) => {
             return actions.payment.execute().then((payment) => {
                 // Do something when payment is successful.
+                window.alert('Thank you for your purchase!');
             });
         }
     };
