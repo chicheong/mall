7c4e60f8594c6a48e245897771a12f42e7b60cd2
chicheong <chicheong@gmail.com>
2019-04-04 17:27:59 +0800

    Development in Progress

---
+++ src/main/java/com/wongs/permission/PermissionsConstants.java
@@ -0,0 +1,20 @@
+package com.wongs.permission;
+
+/**
+ * Constants for Spring Security authorities.
+ */
+public final class PermissionsConstants {
+
+    public static final String CREATE = "C";
+
+    public static final String REARD = "R";
+
+    public static final String UPDATE = "U";
+    
+    public static final String DELETE = "D";
+    
+    public static final String ALL = "CRUD";
+
+    private PermissionsConstants() {
+    }
+}
