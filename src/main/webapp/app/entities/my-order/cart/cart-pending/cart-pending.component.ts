10528dda707e7b904e1ff834cd6505417034849c
chicheong <chicheong@gmail.com>
2019-05-06 17:54:26 +0800

    Development in progress

--- src/main/webapp/app/entities/my-order/cart/cart-pending/cart-pending.component.ts
+++ src/main/webapp/app/entities/my-order/cart/cart-pending/cart-pending.component.ts
@@ -14,8 +14,11 @@ import { CartComponent } from './../../cart.component';
     selector: 'jhi-pending-cart',
     templateUrl: './cart-pending.component.html'
 })
-export class CartPendingComponent extends CartComponent implements OnInit, OnDestroy {
+export class CartPendingComponent implements OnInit, OnDestroy {
 
+    myOrder: MyOrder;
+    subscription: Subscription;
+    eventSubscriber: Subscription;
     isSaving: boolean;
 
     constructor(
@@ -23,11 +26,62 @@ export class CartPendingComponent extends CartComponent implements OnInit, OnDes
         protected myOrderService: MyOrderService,
         protected route: ActivatedRoute,
         protected router: Router
-    ) { super(eventManager, myOrderService, route, router); }
+    ) {}
 
     ngOnInit() {
-        super.ngOnInit();
+        this.subscription = this.route.params.subscribe((params) => {
+            this.load(params['id']);
+        });
+        this.registerChangeInMyOrders();
         this.isSaving = false;
+    }
+    
+    load(id) {
+        this.myOrderService.find(id)
+            .subscribe((myOrderResponse: HttpResponse<MyOrder>) => {
+                this.myOrder = myOrderResponse.body;
+                this.myOrder.items.forEach((item) => {
+                   console.error('item.price: ' + item.price + ', item.quantity: ' + item.quantity);
+                });
+                // Initialize Shipping if not exist
+                if (!this.myOrder) {
+                    console.error('!this.myOrder');
+                } else if (!this.myOrder.shipping) {
+                    const shipping: Shipping = Object.assign(new Shipping());
+                    shipping.status = ShippingStatus.PENDING;
+                    // shipping.currency = this.myOrder.currency;
+                    shipping.price = 0;
+                    const address: Address = Object.assign(new Address());
+                    shipping.shippingAddress = address;
+                    const shippingType: ShippingType = Object.assign(new ShippingType());
+                    shippingType.id = 1;
+                    shippingType.price = 0;
+                    shippingType.currency = this.myOrder.currency;
+                    shipping.type = shippingType;
+                    this.myOrder.shipping = shipping;
+                } else if (!this.myOrder.shipping.shippingAddress || !this.myOrder.shipping.type) {
+                    if (!this.myOrder.shipping.shippingAddress) {
+                        const address: Address = Object.assign(new Address());
+                        this.myOrder.shipping.shippingAddress = address;
+                    }
+                    if (!this.myOrder.shipping.type) {
+                        const shippingType: ShippingType = Object.assign(new ShippingType());
+                        shippingType.id = 1;
+                        shippingType.price = 0;
+                        shippingType.currency = this.myOrder.currency;
+                        this.myOrder.shipping.type = shippingType;
+                    }
+                }
+                // Initialize Payment if not exist
+                if (!this.myOrder) {
+                    console.error('!this.myOrder');
+                } else if (!this.myOrder.payment) {
+                    const payment: Payment = Object.assign(new Payment());
+                    payment.status = PaymentStatus.PENDING;
+                    this.myOrder.payment = payment;
+                }
+                this.cartControl = this.myOrderService.getCartControl(this.myOrder, this.path);
+            });
     }
 
     sumAll(): number {
@@ -63,9 +117,15 @@ export class CartPendingComponent extends CartComponent implements OnInit, OnDes
     }
 
     ngOnDestroy() {
-        super.ngOnDestroy();
         this.subscription.unsubscribe();
         this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    registerChangeInMyOrders() {
+        this.eventSubscriber = this.eventManager.subscribe(
+            'myOrderListModification',
+            (response) => this.load(this.myOrder.id)
+        );
     }
 
     canGoNext() {
