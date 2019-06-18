86f30079888d25a04b71bcc7b16394c5deee11ca
chicheong <chicheong@gmail.com>
2019-06-18 17:49:01 +0800

    Development in progress

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { MallSharedLibsModule, MallSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
+import { FileUploadDialogComponent } from './file-upload/file-upload-dialog.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
     imports: [MallSharedLibsModule, MallSharedCommonModule],
-    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
+    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, FileUploadDialogComponent, FileUploadComponent],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
-    entryComponents: [JhiLoginModalComponent],
+    entryComponents: [JhiLoginModalComponent, FileUploadDialogComponent, FileUploadComponent],
     exports: [MallSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
