8f37c33149484938dafe45fb3a3a951b63bd85ea
kelvinccwong <kelvinccwong@oaitd1367.hkbh.org.hk>
2019-06-26 18:20:32 +0800

    Development in progress

--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -6,9 +6,10 @@ import { NgJhipsterModule } from 'ng-jhipster';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CookieModule } from 'ngx-cookie';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 @NgModule({
-    imports: [NgbModule.forRoot(), InfiniteScrollModule, CookieModule.forRoot(), FontAwesomeModule],
+    imports: [NgbModule.forRoot(), InfiniteScrollModule, CookieModule.forRoot(), FontAwesomeModule, AngularEditorModule],
     exports: [FormsModule, CommonModule, NgbModule, NgJhipsterModule, InfiniteScrollModule, FontAwesomeModule]
 })
 export class MallSharedLibsModule {
